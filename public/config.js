db96f88474405316eb14a091fc48f6521baa411b
x12amsu5 <114162395+x12amsu5@users.noreply.github.com>
2022-09-23 22:03:30 +0800

    Create config.js

---
+++ public/config.js
@@ -0,0 +1,27 @@
+// 配置
+window.Config = {
+
+  // 显示标题
+  SiteName: 'AXGO站点监测',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'ur1864312-c96ee4752efa25c05797f4e9',
+  ],
+
+    // 日志天数
+  // 60 or 90
+  CountDays: 60,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'GitHub',
+      url: 'https://github.com/qikaile/uptime-status'
+    },
+  ],
+};
